657d64fe618690b82b56e4154eb7f1097223c25c
btwwdym <khavrukandriy@gmail.com>
2022-12-08 18:46:38 +0100

    kiel

--- pythonProject/Lab8/zad.py
+++ pythonProject/Lab8/zad.py
@@ -76,5 +76,32 @@ for k in sorted(ch.keys()):
     print(k, ch[k])
 
 
+'''Zadanie 5. Napisz program działający jak prosty kalkulator. W tym celu utwórz funkcje dodawanie(),
+odejmowanie(), mnożenie() oraz dzielenie(). Następnie utwórz słownik, którego kluczem jest działanie, a
+wartością – nazwa odpowiedniej funkcji.
+Uwaga: funkcja jest obiektem, a nazwa funkcji – nazwą (referencją) tego obiektu.'''
+
+
+def dodawanie(num1, num2):
+    return num1 + num2
+def odejmowanie(num1, num2):
+    return num1 - num2
+def mnozenie(num1, num2):
+    return num1 * num2
+def dzielenie(num1, num2):
+    if num2 == 0:
+        print("Nie dziel przez 0!")
+        return "ERROR"
+    return num1 / num2
+slownik={'+': dodawanie, '-': odejmowanie, '*': mnozenie, '/': dzielenie}
+num1=int(input("Podaj pierwszą liczbę: "))
+num2=int(input("Podaj drugą liczbę: "))
+znak=input("Podaj znak działania (+,-,*,/): ")
+if slownik.get(znak, "error")=="error":
+    print("ERROR: znak z poza zakresu!")
+else:
+    print(slownik[znak](num1, num2))
+
+
 
 
