d901a44ff3bf4ac5cb0f58455f033fa36d77d368
Andrii Khavruk <khavrukandriy@gmail.com>
2023-03-21 14:27:59 +0100

    wsiz_main

---
+++ wsiz/script.js
@@ -0,0 +1,18 @@
+/* zad 1 */
+
+var a = 10;
+var b = 20;
+let add= a+b;
+let sub=b-a;
+let mul=a*b;
+let div=b/a;
+let mod=b%a;
+
+//a)
+console.log("add");
+console.log("sub");
+console.log("wynik mnożeni to: "+mul)
+console.log(div)
+console.log(mod);
+
+//b)
---
+++ wsiz/script_to_main1.js
@@ -0,0 +1,7 @@
+const a = 20;
+const b = 10;
+
+console.log(`Dodawanie: ${a + b}`);
+console.log(`Odejmowanie: ${a - b}`);
+console.log(`Mnożenie: ${a * b}`);
+console.log(`Dzielenie: ${a / b}`);
